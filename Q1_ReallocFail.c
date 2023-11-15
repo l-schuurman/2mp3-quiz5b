ad2f2700d608ddc7e7bbedd142fa1d5feb3eefd6
Luke <schuurml@mcmaster.ca>
2023-11-15 10:04:38 -0500

    Quiz5b Files added

---
+++ Q1_ReallocFail.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Fix ptr so that it can be correctly reallocated memory.
+    int arr[] = {2, 6, 14};
+    int i;
+    int* ptr = arr;
+    int* ptr_new;
+
+    // DO NOT CHANGE ANYTHING BELOW THIS LINE
+    ptr_new = (int*)realloc(ptr, sizeof(int) * 4);
+    *(ptr_new + 3) = 30;
+
+    for (i = 0; i < 4; i++)
+        printf("%d ", *(ptr_new + i));
+    printf("\n");
+    return 0;
+}
---
+++ Q2_ReallocDecimation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+
+    int* soldiers = NULL;
+
+    /*
+     Implement your logic here!
+     Declare whatever variables you need
+     Make sure to use the soldiers pointer!!!!!
+    */
+
+    // -----------------------
+
+    // PRINT THE SOLDIERS
+
+    printf("\n");
+
+    // PRINT THE SUM OF DECIMATED SOLDIERS AGES
+
+    printf("\n");
+
+    return 0;
+}
